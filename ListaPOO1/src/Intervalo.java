1de7a88ea55d0f65f7c57a95bcf07dee5b769959
Vander Reis <77352309+Vander-Reis@users.noreply.github.com>
2021-03-11 18:05:59 -0300

    Rename Main1024.java to Intervalo.java

--- ListaPOO1/src/Intervalo.java
+++ ListaPOO1/src/Intervalo.java
@@ -1,8 +1,8 @@
-//Você deve fazer um programa que leia um valor qualquer e apresente 
+//VocÃª deve fazer um programa que leia um valor qualquer e apresente 
 //uma mensagem dizendo em qual dos seguintes intervalos 
 //([0,25], (25,50], (50,75], (75,100]) este valor se encontra. 
-//Obviamente se o valor não estiver em nenhum 
-//destes intervalos, deverá ser impressa a mensagem “Fora de intervalo”.
+//Obviamente se o valor nÃ£o estiver em nenhum 
+//destes intervalos, deverÃ¡ ser impressa a mensagem â€œFora de intervaloâ€.
 
 import java.util.Scanner;
 
